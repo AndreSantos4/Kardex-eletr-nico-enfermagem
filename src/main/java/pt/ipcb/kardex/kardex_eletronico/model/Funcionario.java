c8e88ad1cd30eaeb00341deadbeed73a685e8f8d
DanielPadre58 <2kupwhite@gmail.com>
2026-03-17 23:14:34 +0000

    feat: Creation of the entity "Funcionario"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Funcionario.java
@@ -0,0 +1,28 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "funcionario")
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+    
+    @JoinColumn(name = "id_utilizador", nullable = false)
+    @OneToOne(targetEntity = Utilizador.class)
+    public Utilizador utilizador;
+    
+    @ManyToMany(targetEntity = Turno.class)
+    public Set<Turno> turnos;
+}
