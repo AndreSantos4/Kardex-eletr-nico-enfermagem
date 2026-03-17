655a1ed3691fcb3cecf8e78f1d9615ef66ef1e18
DanielPadre58 <2kupwhite@gmail.com>
2026-03-17 23:14:48 +0000

    feat: Creation of the entity "Registo"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Registo.java
@@ -0,0 +1,37 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "registo")
+public class Registo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+    
+    @JoinColumn(name = "id_utilizador", nullable = false)
+    @ManyToOne(targetEntity = Utilizador.class)
+    public Utilizador utilizador;
+    
+    @Column(name = "nivel_registo", nullable = false)
+    @Enumerated(EnumType.STRING)
+    public NivelRegisto nivelRegisto;
+    
+    @Column(name = "tipo_registo", nullable = false)
+    @Enumerated(EnumType.STRING)
+    public TipoRegisto tipoRegisto;
+    
+    public String tabelaOrigem;
+    
+    public String mensagem;
+    
+    public String detalhes;
+}
