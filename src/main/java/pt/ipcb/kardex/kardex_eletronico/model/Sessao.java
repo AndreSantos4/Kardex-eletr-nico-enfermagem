8f9c225cf83880b47b7c2efd82a6fc941c75f832
DanielPadre58 <2kupwhite@gmail.com>
2026-03-17 22:12:35 +0000

    feat: Creation of the entity "Sessao"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Sessao.java
@@ -0,0 +1,26 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "sessao")
+public class Sessao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+    
+    @JoinColumn(name = "utilizador", nullable = false)
+    @OneToOne(targetEntity = Utilizador.class)
+    public Utilizador utilizador;
+    
+    @Column(name = "endereco_ip", nullable = false)
+    public String enderecoIP;
+}
