a474e4ae5c631b5452cc84dcee5c630a3949a997
DanielPadre58 <2kupwhite@gmail.com>
2026-03-17 23:14:59 +0000

    feat: Creation of the entity "Turno"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Turno.java
@@ -0,0 +1,31 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "turno")
+public class Turno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+    
+    @Column(name = "inicio", nullable = false)
+    public LocalDateTime inicio;
+    
+    @Column(name = "fim", nullable = false)
+    public LocalDateTime fim;
+    
+    @ManyToMany(targetEntity = Funcionario.class)
+    public Set<Funcionario> funcionariosAlocados; 
+}
