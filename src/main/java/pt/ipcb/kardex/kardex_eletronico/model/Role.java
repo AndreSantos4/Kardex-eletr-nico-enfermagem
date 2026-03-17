ebe4ac86d32dc97639381bed08cd6ca224859571
DanielPadre58 <2kupwhite@gmail.com>
2026-03-17 23:19:12 +0000

    feat: Added name field

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Role.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Role.java
@@ -1,8 +1,14 @@
 ﻿package pt.ipcb.kardex.kardex_eletronico.model;
 
 public enum Role {
-    ADMIN,
-    MEDICO,
-    ENFERMEIRO_CHEFE,
-    ENFERMEIRO
+    ADMIN("Administrador"),
+    MEDICO("Médico"),
+    ENFERMEIRO_CHEFE("Enfermeiro Chefe"),
+    ENFERMEIRO("Enfermeiro");
+    
+    private final String nome;
+    
+    Role(String nome) {
+        this.nome = nome;
+    }
 }
