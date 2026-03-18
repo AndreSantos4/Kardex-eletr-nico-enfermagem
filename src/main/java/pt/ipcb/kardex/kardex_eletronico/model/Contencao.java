290da84d62202a890c4e2130c495750cafe1bd53
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:35:11 +0000

    feat: Creation of the entity "Contencao"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Contencao.java
@@ -0,0 +1,36 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "contencao")
+public class Contencao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;        
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Funcionario medico;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Medicamento medicamento;
+    
+    @Column(name = "justificacao", nullable = false)
+    public String justificacao;
+    
+    @Column(name = "dose", nullable = false)
+    public Long dose;
+    
+    @Column(name = "duracao", nullable = false)
+    public int duracao;
+    
+    @Column(name = "data", nullable = false)
+    public LocalDate data;
+}
