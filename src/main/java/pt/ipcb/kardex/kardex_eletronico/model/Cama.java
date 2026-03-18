a2162ab1e4c051dc3742343afcbd06b4570f161f
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:34:05 +0000

    feat: Creation of the entity "Cama"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Cama.java
@@ -0,0 +1,24 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cama")
+public class Cama {
+    @Id
+    public String id;
+    
+    @Column(name = "ocupada", nullable = false)
+    public Boolean ocupada;
+}
