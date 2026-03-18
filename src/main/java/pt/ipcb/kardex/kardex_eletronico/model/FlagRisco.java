a8105cddf8748ddd310c3fd4b6b5293d409250b9
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:35:40 +0000

    feat: Creation of the entity "FlagRisco"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/FlagRisco.java
@@ -0,0 +1,25 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "flag_risco")
+public class FlagRisco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String id;
+
+    @Column(name = "nome", nullable = false)
+    public String nome;
+
+    @Column(name = "descricao")
+    public String descricao;
+}
