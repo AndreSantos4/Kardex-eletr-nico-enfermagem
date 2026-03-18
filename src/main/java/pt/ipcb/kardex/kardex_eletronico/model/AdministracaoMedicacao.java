017871afa0df50f3da4b1cf51857a12bdb13a1f5
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:33:26 +0000

    feat: Creation of the entity "AdministracaoMedicacao"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/AdministracaoMedicacao.java
@@ -0,0 +1,36 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.security.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "administracao_medicacao")
+public class AdministracaoMedicacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    public Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Prescricao prescricao;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Funcionario funcionario;
+    
+    @Column(name = "observacoes")
+    public String observacoes;
+    
+    @Column(name = "foi_administrado")
+    public Boolean administrado;
+    
+    @Column(name = "timestamp")
+    public Timestamp timestamp;
+}
