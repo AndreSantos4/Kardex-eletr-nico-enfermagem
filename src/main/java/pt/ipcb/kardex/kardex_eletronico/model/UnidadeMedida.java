1b3a6b75745482530f70314ccd56a8f27f0a3f59
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:42:06 +0000

    feat: Creation of support metric unit enum "UnidadeMedida"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/UnidadeMedida.java
@@ -0,0 +1,14 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+public enum UnidadeMedida {
+    GRAMAS("g"),
+    MILIGRAMAS("mg"),
+    MICROGRAMAS("μg"),
+    MILILITROS("ml");
+    
+    public final String simbolo;
+    
+    UnidadeMedida(String simbolo) {
+        this.simbolo = simbolo;
+    }
+}
