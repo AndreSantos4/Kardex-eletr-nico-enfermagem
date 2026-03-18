414dc0b671d14a2c95c5dc9ffc7bf0eb592d45aa
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:37:01 +0000

    feat: Creation of the entity "RegistoClinico"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/RegistoClinico.java
@@ -0,0 +1,31 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "registo_clinico")
+public class RegistoClinico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    
+    @Column(name = "timestamp", nullable = false)
+    public Timestamp timestamp;
+    
+    @ManyToOne(targetEntity = Funcionario.class)
+    public Funcionario funcionario;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Utente utente;
+}
