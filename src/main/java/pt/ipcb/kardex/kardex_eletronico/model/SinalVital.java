510d2bd05e7bf5fa039748a0c818ad3ef1c9c4a5
DanielPadre58 <2kupwhite@gmail.com>
2026-03-18 21:37:53 +0000

    feat: Creation of the entity "SinalVital"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/SinalVital.java
@@ -0,0 +1,50 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "sinal_vital")
+public class SinalVital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Funcionario funcionario;
+    
+    @Column(name = "tensao_arterial_distolica", nullable = false)
+    public Integer tensaoArteriaDistolica;
+    
+    @Column(name = "tensao_arterial_sistolica", nullable = false)
+    public Integer tensaoArteriaSistolica;
+    
+    @Column(name = "frequencia_cardiaca", nullable = false)
+    public Integer frequenciaCardiaca;
+    
+    @Column(name = "temperatura", nullable = false)
+    public int temperatura;
+    
+    @Column(name = "spo2", nullable = false)
+    public int spo2;
+    
+    @Column(name = "dor")
+    public Short dor;
+    
+    @Column(name = "glicemia", nullable = false)
+    public Integer glicemia;
+    
+    @Column(name = "observacoes")
+    public String observacoes;
+    
+    public Timestamp data;
+}
