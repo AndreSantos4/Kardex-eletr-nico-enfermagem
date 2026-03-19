c82edca0943ef8d64bfcb580c6e43130305f4188
DanielPadre58 <2kupwhite@gmail.com>
2026-03-19 10:30:08 +0000

    feat: Creation of the support Incidente gravity enum "GravidadeIncidente"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/GravidadeIncidente.java
@@ -0,0 +1,14 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+public enum GravidadeIncidente {
+    BAIXA("Baixa"),
+    MODERADA("Moderada"),
+    NORMAL("Normal"),
+    ALTA("Alta");
+    
+    public final String nome;
+    
+    GravidadeIncidente(String nome) {
+        this.nome = nome;
+    }
+}
