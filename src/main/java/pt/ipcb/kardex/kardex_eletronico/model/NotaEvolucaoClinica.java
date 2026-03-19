cc465e954c643079f80dd5cf056552541182d7cb
DanielPadre58 <2kupwhite@gmail.com>
2026-03-19 10:28:19 +0000

    feat: Creation of entity "NotaEvolucaoClinica"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/NotaEvolucaoClinica.java
@@ -0,0 +1,37 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "nota_evolucao_clinica")
+public class NotaEvolucaoClinica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public ProcessoClinico processoClinico;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Funcionario medico;
+    
+    @Column(name = "tipo_nota", nullable = false)
+    @Enumerated(EnumType.STRING)
+    public TipoNotaEvolucaoClinica tipo;
+    
+    @Column(name = "descricao")
+    public String descricao;
+    
+    @Column(name = "data_emitida", nullable = false)
+    public LocalDateTime dataEmitida;
+}
