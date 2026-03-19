4b3892de2927f6923f12a859beaa0898b6d3dcdf
DanielPadre58 <2kupwhite@gmail.com>
2026-03-19 23:42:00 +0000

    feat: Implemented service AuthorizationService that handles user authentication business logic

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/AuthorizationService.java
@@ -0,0 +1,22 @@
+package pt.ipcb.kardex.kardex_eletronico.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import pt.ipcb.kardex.kardex_eletronico.repository.UtilizadorRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UtilizadorRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String numeroMecanograficoString) throws UsernameNotFoundException {
+        var numeroMecanografico = Long.parseLong(numeroMecanograficoString);
+        return repository.findbyNumeroMecanografico(numeroMecanografico);
+    }
+}
