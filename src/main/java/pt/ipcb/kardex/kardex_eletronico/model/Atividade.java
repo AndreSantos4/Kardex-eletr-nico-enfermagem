1e6327b451e04249dd47aba2c453c6939f177dcf
DanielPadre58 <2kupwhite@gmail.com>
2026-03-19 10:27:52 +0000

    feat: Creation of entity "Atividade"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Atividade.java
@@ -0,0 +1,40 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cama")
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public PlanoCuidados planoCuidados;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Funcionario funcionario;
+    
+    @Column(name = "nome", nullable = false)
+    public String nome;
+    
+    @Column(name = "foi_realizada")
+    public Boolean realizada;
+    
+    @Column(name = "data_inicio", nullable = false)
+    public LocalDateTime dataInicio;
+    
+    @Column(name = "data_realizacao")
+    public Timestamp dataRealizacao;
+}
