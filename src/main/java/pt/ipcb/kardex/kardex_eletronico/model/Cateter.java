4b57b091fe72fddfd544d4af95406c8b5bd4bcac
DanielPadre58 <2kupwhite@gmail.com>
2026-03-19 10:27:59 +0000

    feat: Creation of entity "Cateter"

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/Cateter.java
@@ -0,0 +1,46 @@
+﻿package pt.ipcb.kardex.kardex_eletronico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cateter")
+public class Cateter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public ProcessoClinico processoClinico;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Funcionario funcionario;
+    
+    @Column(name = "calibre_cateter", nullable = false)
+    @Enumerated(EnumType.STRING)
+    public CalibreCateter calibre;
+    
+    @Column(name = "data_insercao", nullable = false)
+    public LocalDateTime dataInsercao;
+    
+    @Column(name = "data_substituicao")
+    public LocalDateTime dataSubstituicao;
+    
+    @Column(name = "local_insercao", nullable = false)
+    public String localInsercao;
+    
+    @Column(name = "estado_cateter", nullable = false)
+    public EstadoCateter estado;
+    
+    @Column(name = "observacoes")
+    public String observacoes;
+}
