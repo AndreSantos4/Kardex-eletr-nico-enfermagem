073f58ca8e34a6eed412a1e6013fe741ce2714e5
Daniel Padre <2kupwhite@gmail.com>
2026-03-23 22:42:54 -0100

    feat: Gets the user JWT token from a cookie

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/security/SecurityFilter.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/security/SecurityFilter.java
@@ -2,33 +2,36 @@ package pt.ipcb.kardex.kardex_eletronico.security;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.repository.UtilizadorRepository;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private TokenService service;
-    @Autowired
-    private UtilizadorRepository repository;
+    private final TokenService service;
+    private final UtilizadorRepository repository;
+
+    public SecurityFilter(TokenService service, UtilizadorRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if(token != null){
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var token = recoverCookie(request);
+        if (token != null) {
             var subject = service.validateToken(token);
             UserDetails user = repository.findByNumeroMecanografico(subject);
 
@@ -39,10 +42,16 @@ public class SecurityFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request){
-        var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
+    public String recoverCookie(HttpServletRequest request) {
+        var cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("kardex-cookie")) {
+                    return cookie.getValue();
+                }
+            }
+        }
 
-        return authHeader.replace("Bearer ", "");
-    } 
+        return null;
+    }
 }
