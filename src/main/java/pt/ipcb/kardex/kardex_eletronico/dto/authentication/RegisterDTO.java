a77a2c21bad52467d8e4ffdfa7a576419e8e806c
Daniel Padre <2kupwhite@gmail.com>
2026-03-23 22:38:02 -0100

    featt: Added necessary user fields to the Register DTO

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/authentication/RegisterDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/authentication/RegisterDTO.java
@@ -6,6 +6,10 @@ import pt.ipcb.kardex.kardex_eletronico.model.enumerated.Role;
 
 public record RegisterDTO(
     @JsonProperty("numeroMecanografico") Long numeroMecanografico, 
-    @JsonProperty("role") Role role) {
-    
+    @JsonProperty("role") Role role,
+    @JsonProperty("email") String email,
+    @JsonProperty("nome") String nome,
+    @JsonProperty("sexo") char sexo,
+    @JsonProperty("contacto") int contacto,
+    @JsonProperty("contactoEmergencia") int contactoEmergencia) {
 }
