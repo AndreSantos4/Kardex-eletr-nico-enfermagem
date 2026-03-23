000b18d7aea030a4235b0a379e63c300ce113b42
Daniel Padre <2kupwhite@gmail.com>
2026-03-23 22:44:09 -0100

    chore: Changed implementation of DI

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/auth/AuthorizationService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/auth/AuthorizationService.java
@@ -1,18 +1,18 @@
-package pt.ipcb.kardex.kardex_eletronico.service;
+package pt.ipcb.kardex.kardex_eletronico.service.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.repository.UtilizadorRepository;
 
 @Service
+@RequiredArgsConstructor
 public class AuthorizationService implements UserDetailsService {
 
-    @Autowired
-    UtilizadorRepository repository;
+    private final UtilizadorRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String numeroMecanograficoString) throws UsernameNotFoundException {
