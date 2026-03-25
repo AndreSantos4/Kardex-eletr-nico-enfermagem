7bbc0fa6d607fa095f3a55c6cbe90efd3375169f
Daniel Padre <2kupwhite@gmail.com>
2026-03-25 18:34:51 -0100

    feat: Added business logic to the new endpoints

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/IUserService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/IUserService.java
@@ -1,9 +1,16 @@
 package pt.ipcb.kardex.kardex_eletronico.service;
 
-import pt.ipcb.kardex.kardex_eletronico.dto.UtilizadorDTO;
+import java.util.List;
+import java.util.Optional;
+
+import jakarta.servlet.http.HttpServletRequest;
+import pt.ipcb.kardex.kardex_eletronico.dto.user.UpdateUserDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.user.UtilizadorDTO;
 
 public interface IUserService {
-    void updateUser(Long id, UtilizadorDTO data);
+    List<UtilizadorDTO> getAllUsers(Optional<String> filter);
+    void updateUser(Long id, UpdateUserDTO data);
     void deactivateUser(Long id);
     UtilizadorDTO getUserById(Long id);
+    UtilizadorDTO getUserByToken(HttpServletRequest request);
 }
