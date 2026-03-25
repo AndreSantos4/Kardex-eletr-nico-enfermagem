94de0c369ab1eef1d978cf24839a42a170d1428d
Daniel Padre <2kupwhite@gmail.com>
2026-03-25 22:43:14 -0100

    feat: add incidente dto to store incident data

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/worker/IncidenteDTO.java
@@ -0,0 +1,18 @@
+package pt.ipcb.kardex.kardex_eletronico.dto.worker;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import pt.ipcb.kardex.kardex_eletronico.model.enumerated.GravidadeIncidente;
+
+public record IncidenteDTO(
+    @JsonProperty("id") Long id,
+    @JsonProperty("descricao") String descricao,
+    @JsonProperty("funcionario") FuncionarioDTO funcionario,
+    @JsonProperty("gravidade") GravidadeIncidente gravidade,
+    @JsonProperty("medidasTomadas") String medidasTomadas,
+    @JsonProperty("data") LocalDate data
+) {
+
+}
