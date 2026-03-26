48744df29daacc0b79f5c88c4b63918ecf94a70a
Daniel Padre <2kupwhite@gmail.com>
2026-03-26 22:31:41 -0100

    feat: add dto to store medicamento entity data

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/stock/MedicamentoDTO.java
@@ -0,0 +1,24 @@
+package pt.ipcb.kardex.kardex_eletronico.dto.stock;
+
+import pt.ipcb.kardex.kardex_eletronico.dto.patient.AlergiaDTO;
+import pt.ipcb.kardex.kardex_eletronico.model.enumerated.UnidadeMedida;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record MedicamentoDTO(
+    @JsonProperty("id") Long id,
+    @JsonProperty("nome") String nome,
+    @JsonProperty("principioAtivo") String principioAtivo,
+    @JsonProperty("formaFarmaceutica") String formaFarmaceutica,
+    @JsonProperty("quantidade") Long quantidade,
+    @JsonProperty("unidadeMedida") UnidadeMedida unidade,
+    @JsonProperty("dataValidade") LocalDate dataValidade,
+    @JsonProperty("viasAdministracao") List<ViaAdministracaoDTO> viasAdministracao,
+    @JsonProperty("alertas") List<AlertaDTO> alertas,
+    @JsonProperty("alergiasIncompativeis") List<AlergiaDTO> alergiaIncompativeis
+) {
+
+}
