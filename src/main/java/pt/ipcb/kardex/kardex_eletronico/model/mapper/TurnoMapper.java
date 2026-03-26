dfdaf8b1733f505ad8e00f246e6b21ac5d9e0cc6
Daniel Padre <2kupwhite@gmail.com>
2026-03-26 17:51:48 -0100

    feat: add mapping from CreatehiftDTO to entity

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/TurnoMapper.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/TurnoMapper.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 
-import pt.ipcb.kardex.kardex_eletronico.dto.worker.TurnoDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.TurnoDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Turno;
 
 @Mapper(componentModel = "spring")
@@ -12,6 +13,8 @@ public interface TurnoMapper {
     TurnoDTO toDTO(Turno turno);
 
     Turno toEntity(TurnoDTO dto);
+
+    Turno fromCreate(CreateShiftDTO data);
 
     List<TurnoDTO> toDTOList(List<Turno> turnos);
 }
