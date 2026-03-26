7f45948ffede86552303d9ccdcf7482d15d50a34
Daniel Padre <2kupwhite@gmail.com>
2026-03-26 22:40:14 -0100

    chore: add repository annotation

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/WorkerRepository.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/WorkerRepository.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Funcionario;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Turno;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Funcionario, Long> {
 
     @Query("SELECT t FROM Funcionario f JOIN f.turnos t WHERE f.id = :id AND t.inicio >= :from AND t.inicio < :to ORDER BY t.inicio ASC")
