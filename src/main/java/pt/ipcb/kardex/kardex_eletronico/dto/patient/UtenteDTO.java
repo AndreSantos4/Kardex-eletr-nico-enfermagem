0454aac1a81c7f5b55ba09b4b30a83d27d6445b4
Daniel Padre <2kupwhite@gmail.com>
2026-03-26 22:28:06 -0100

    feat: add dto to store utente data

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/patient/UtenteDTO.java
@@ -0,0 +1,23 @@
+package pt.ipcb.kardex.kardex_eletronico.dto.patient;
+
+import pt.ipcb.kardex.kardex_eletronico.model.enumerated.Sexo;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UtenteDTO(
+    @JsonProperty("id") Long id,
+    @JsonProperty("numeroSNS") int numeroSNS,
+    @JsonProperty("numeroCC") String numeroCC,
+    @JsonProperty("primerioNome") String primeiroNome,
+    @JsonProperty("apelido") String apelido,
+    @JsonProperty("sexo") Sexo sexo,
+    @JsonProperty("contacto") int contacto,
+    @JsonProperty("contactoEmergencia") int contactoEmergencia,
+    @JsonProperty("estado") Long estado,
+    @JsonProperty("alergias") List<AlergiaDTO> alergias,
+    @JsonProperty("flags") List<FlagRiscoDTO> flagsRiscos
+) {
+
+}
