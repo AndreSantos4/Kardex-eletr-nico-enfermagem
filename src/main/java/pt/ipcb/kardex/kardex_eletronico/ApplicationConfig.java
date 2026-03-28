0c73df9f372b0c51348908e1b01b1b48608c3a79
Daniel Padre <2kupwhite@gmail.com>
2026-03-28 22:25:27 -0100

    feat: add configuration class for general app config

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package pt.ipcb.kardex.kardex_eletronico;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
