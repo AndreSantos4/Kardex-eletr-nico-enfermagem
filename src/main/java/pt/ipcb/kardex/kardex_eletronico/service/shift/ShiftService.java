968fe174ee7a508fd8964e3eaf34e8b31d9ed377
Daniel Padre <2kupwhite@gmail.com>
2026-03-27 23:54:39 -0100

    feat: add create incident operation business logic

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/shift/ShiftService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/shift/ShiftService.java
@@ -1,8 +1,12 @@
 package pt.ipcb.kardex.kardex_eletronico.service.shift;
 
+import jakarta.servlet.http.HttpServletRequest;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateIncidentDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
 
 public interface ShiftService {
     
     void CreateShift(CreateShiftDTO data);
+
+    void createIncident(CreateIncidentDTO data, HttpServletRequest request);
 }
