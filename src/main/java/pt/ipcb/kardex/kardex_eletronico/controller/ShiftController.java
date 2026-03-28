9e8a916737295e8001e2757d962f87bfa3247c9a
Daniel Padre <2kupwhite@gmail.com>
2026-03-27 23:15:03 -0100

    feat: add create incident endpoint

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/ShiftController.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/ShiftController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.controller.config.ApiResponse;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateIncidentDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
 import pt.ipcb.kardex.kardex_eletronico.service.shift.ShiftService;
 
@@ -22,5 +24,11 @@ public class ShiftController{
     public ResponseEntity<ApiResponse<?>> createShift(@RequestBody CreateShiftDTO data){
         service.CreateShift(data);
         return ResponseEntity.ok(ApiResponse.ok("Turno criado com sucesso", null));
+    }
+
+    @PostMapping("/incidents")
+    public ResponseEntity<ApiResponse<?>> createIncident(@RequestBody CreateIncidentDTO data, HttpServletRequest request){
+        service.createIncident(data, request);
+        return ResponseEntity.ok(ApiResponse.ok("Incidente registado com sucesso", null));
     }
 }
