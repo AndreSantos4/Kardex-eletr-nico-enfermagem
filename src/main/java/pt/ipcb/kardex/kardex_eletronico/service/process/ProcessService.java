4e968e228d29e6436173d87f6517e7ee62f0ea43
Daniel Padre <2kupwhite@gmail.com>
2026-03-27 23:52:31 -0100

    feat: add create administration operation businesss logic

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/process/ProcessService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/process/ProcessService.java
@@ -1,9 +1,13 @@
 package pt.ipcb.kardex.kardex_eletronico.service.process;
 
+import jakarta.servlet.http.HttpServletRequest;
+import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreateAdministrationDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreatePrescriptionDTO;
 
 public interface ProcessService {
 
     void createPrescription(Long processId, CreatePrescriptionDTO data);
+
+    void administrateMedication(Long prescriptionId, CreateAdministrationDTO data, HttpServletRequest request);
 
 }
--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/process/ProcessServiceImpl.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/process/ProcessServiceImpl.java
@@ -2,13 +2,18 @@ package pt.ipcb.kardex.kardex_eletronico.service.process;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreateAdministrationDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreatePrescriptionDTO;
 import pt.ipcb.kardex.kardex_eletronico.exception.EntityNotFoundException;
+import pt.ipcb.kardex.kardex_eletronico.model.mapper.AdministracaoMapper;
 import pt.ipcb.kardex.kardex_eletronico.model.mapper.PrescricaoMapper;
+import pt.ipcb.kardex.kardex_eletronico.repository.AdministracaoRepository;
 import pt.ipcb.kardex.kardex_eletronico.repository.MedicamentoRepository;
 import pt.ipcb.kardex.kardex_eletronico.repository.PrescricaoRepository;
 import pt.ipcb.kardex.kardex_eletronico.repository.ProcessoClinicoRepository;
+import pt.ipcb.kardex.kardex_eletronico.service.worker.WorkerService;
 
 @Service
 @RequiredArgsConstructor
@@ -18,11 +23,15 @@ public class ProcessServiceImpl implements ProcessService{
     private final MedicamentoRepository medicamentoRepository;
     private final PrescricaoMapper prescricaoMapper;
     private final PrescricaoRepository prescricaoRepository;
+    private final AdministracaoRepository administracaoRepository;
+    private final AdministracaoMapper administracaoMapper;
+
+    private final WorkerService workerService;
 
     @Override
     public void createPrescription(Long processId, CreatePrescriptionDTO data) {
         var process = repository.findById(processId)
-            .orElseThrow(() -> EntityNotFoundException.forId(data.idMedicamento(), "Processo"));
+            .orElseThrow(() -> EntityNotFoundException.forId(processId, "Processo"));
         var medication = medicamentoRepository.findById(data.idMedicamento())
             .orElseThrow(() -> EntityNotFoundException.forId(data.idMedicamento(), "Medicamento"));
 
@@ -31,5 +40,20 @@ public class ProcessServiceImpl implements ProcessService{
         prescription.setProcesso(process);
         
         prescricaoRepository.save(prescription);
+    }
+
+    @Override
+    public void administrateMedication(Long prescriptionId, CreateAdministrationDTO data, HttpServletRequest request) {
+        var prescription = prescricaoRepository.findById(prescriptionId)
+            .orElseThrow(() -> EntityNotFoundException.forId(prescriptionId, "Prescrição"));
+        var worker = workerService.getAutenticatedWorker(request);
+        var shift = workerService.getCurrentShift(worker.getId());
+
+        var administration = administracaoMapper.fromCreate(data);
+        administration.setPrescricao(prescription);
+        administration.setFuncionario(worker);
+        administration.setTurno(shift);
+
+        administracaoRepository.save(administration);
     }
 }
