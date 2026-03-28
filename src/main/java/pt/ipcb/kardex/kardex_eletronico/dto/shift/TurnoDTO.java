61175eb46e87a926c26c34bb329bf9ee2519da68
Daniel Padre <2kupwhite@gmail.com>
2026-03-27 23:32:27 -0100

    feat: add fim field and change from LocalDate to LocalDateTime plus formatting

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/TurnoDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/TurnoDTO.java
@@ -1,16 +1,15 @@
 package pt.ipcb.kardex.kardex_eletronico.dto.shift;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import pt.ipcb.kardex.kardex_eletronico.dto.worker.IncidenteDTO;
-
 public record TurnoDTO(
     @JsonProperty("id") Long id,
     @JsonProperty("nome") String nome,
-    @JsonProperty("inicio") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate inicio,
+    @JsonProperty("inicio") @JsonFormat(pattern = "dd/MM/yyyy:HH:mm:ss") LocalDateTime inicio,
+    @JsonProperty("fim") @JsonFormat(pattern = "dd/MM/yyyy:HH:mm:ss") LocalDateTime fim,
     @JsonProperty("incidentes") IncidenteDTO[] incidentes
 ) {
     
