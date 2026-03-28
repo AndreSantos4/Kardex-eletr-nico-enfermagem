caf56b359a267b64f263da81952b35a118bc71a3
Daniel Padre <2kupwhite@gmail.com>
2026-03-27 23:32:56 -0100

    chore: add formatting

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/stock/MedicamentoDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/stock/MedicamentoDTO.java
@@ -6,6 +6,7 @@ import pt.ipcb.kardex.kardex_eletronico.model.enumerated.UnidadeMedida;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record MedicamentoDTO(
@@ -15,7 +16,7 @@ public record MedicamentoDTO(
     @JsonProperty("formaFarmaceutica") String formaFarmaceutica,
     @JsonProperty("quantidade") Long quantidade,
     @JsonProperty("unidadeMedida") UnidadeMedida unidade,
-    @JsonProperty("dataValidade") LocalDate dataValidade,
+    @JsonProperty("dataValidade") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataValidade,
     @JsonProperty("viasAdministracao") List<ViaAdministracaoDTO> viasAdministracao,
     @JsonProperty("alertas") List<AlertaDTO> alertas,
     @JsonProperty("alergiasIncompativeis") List<AlergiaDTO> alergiaIncompativeis
