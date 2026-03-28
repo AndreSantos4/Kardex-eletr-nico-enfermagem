0124001ff6f74d11968a23f0aa873ceb3d8fbd86
Daniel Padre <2kupwhite@gmail.com>
2026-03-27 23:37:50 -0100

    chore: add mapper annotation

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/AdministracaoMapper.java
@@ -0,0 +1,20 @@
+package pt.ipcb.kardex.kardex_eletronico.model.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import pt.ipcb.kardex.kardex_eletronico.dto.prescription.AdministracaoDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreateAdministrationDTO;
+import pt.ipcb.kardex.kardex_eletronico.model.entity.AdministracaoMedicacao;
+
+@Mapper(componentModel = "spring")
+public interface AdministracaoMapper {
+    AdministracaoDTO toDTO(AdministracaoMedicacao prescricao);
+
+    AdministracaoMedicacao toEntity(AdministracaoDTO dto);
+
+    AdministracaoMedicacao fromCreate(CreateAdministrationDTO dto);
+
+    List<AdministracaoDTO> toDTOList(List<AdministracaoMedicacao> prescricoes);
+}
