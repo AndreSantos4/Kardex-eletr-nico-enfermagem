1754454153f8eb14d00ffb7f4d48e64f4df4b7c4
Daniel Padre <2kupwhite@gmail.com>
2026-03-29 22:37:53 +0000

    feat: add service perform business logic on kardex monitoring operations

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/stats/StatsService.java
@@ -0,0 +1,8 @@
+package pt.ipcb.kardex.kardex_eletronico.service.stats;
+
+import pt.ipcb.kardex.kardex_eletronico.dto.stats.KardexCountsDTO;
+
+public interface StatsService {
+
+    KardexCountsDTO getCounts();
+}
