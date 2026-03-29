4a4457a1bc0a82c8beaac269d8953bd24790b757
Daniel Padre <2kupwhite@gmail.com>
2026-03-29 21:01:02 +0000

    feat: add repository to interact with Password reset request table

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/PasswordResetRequestRepository.java
@@ -0,0 +1,15 @@
+package pt.ipcb.kardex.kardex_eletronico.repository;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pt.ipcb.kardex.kardex_eletronico.model.entity.PasswordResetRequest;
+
+@Repository
+public interface PasswordResetRequestRepository extends JpaRepository<PasswordResetRequest, Long> {
+
+	void deleteAllByPedidoEmBefore(LocalDateTime cutoff);
+
+}
