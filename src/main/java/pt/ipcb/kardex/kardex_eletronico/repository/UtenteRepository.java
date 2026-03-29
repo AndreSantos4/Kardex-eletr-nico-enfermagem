274b3d30ef47c3c4642c6ac39a9142cfb0acb855
Daniel Padre <2kupwhite@gmail.com>
2026-03-29 22:35:15 +0000

    feat: add query to get num of hospitalized patients

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/UtenteRepository.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/UtenteRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Utente;
+import pt.ipcb.kardex.kardex_eletronico.model.enumerated.EstadoUtente;
 
 @Repository
 public interface UtenteRepository extends JpaRepository<Utente, Long>{
+
+    long countByEstado(EstadoUtente internado);
 
 }
