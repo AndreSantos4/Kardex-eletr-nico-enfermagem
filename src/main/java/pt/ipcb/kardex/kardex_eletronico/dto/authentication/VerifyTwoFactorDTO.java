9d5eea237bce9b5eadd48d75b302f1ab02cddb1e
Daniel Padre <2kupwhite@gmail.com>
2026-03-30 00:56:21 +0000

    feat: add dto to store 2FA verification data

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/authentication/VerifyTwoFactorDTO.java
@@ -0,0 +1,10 @@
+package pt.ipcb.kardex.kardex_eletronico.dto.authentication;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record VerifyTwoFactorDTO(
+    @JsonProperty("numeroMecanografico") Long numeroMecanografico,
+    @JsonProperty("codigo") String codigo
+) {
+
+}
