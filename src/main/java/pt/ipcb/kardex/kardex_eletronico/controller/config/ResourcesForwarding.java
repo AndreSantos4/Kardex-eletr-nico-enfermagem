8a974bd29fe19e8d3cf187d8eff4f085917b1932
1tsDan <2kupwhite@gmail.com>
2026-04-02 01:35:36 +0000

    fix: change from @RestController to @Controller

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/config/ResourcesForwarding.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/config/ResourcesForwarding.java
@@ -1,11 +1,12 @@
 package pt.ipcb.kardex.kardex_eletronico.controller.config;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("")
 public class ResourcesForwarding {
 
