2433b110741d3e3d7844ed2ecc96f3ddfbe2a99b
1tsDan <2kupwhite@gmail.com>
2026-04-03 19:34:26 +0000

    feat: add new query

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/ProcessoClinicoRepository.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/ProcessoClinicoRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pt.ipcb.kardex.kardex_eletronico.model.entity.ProcessoClinico;
+import pt.ipcb.kardex.kardex_eletronico.model.entity.Utente;
 
 @Repository
 public interface ProcessoClinicoRepository extends JpaRepository<ProcessoClinico, Long>{
+
+    boolean existsByUtente(Utente patient);
 
 }
