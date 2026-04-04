d7237d5974da1c405bd2a9a426f9350b1da1d1e0
AndreSantos4 <149972193+AndreSantos4@users.noreply.github.com>
2026-04-04 15:39:30 +0100

    feat: add user role validation and redirect to appropriate page

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -25,9 +25,7 @@ function showErrorWithCountdown() {
 function validateForm(event) {
   event.preventDefault();
 
-  numeroMecanografico = document.getElementById(
-    "numero_mecanografico",
-  ).value;
+  numeroMecanografico = document.getElementById("numero_mecanografico").value;
 
   const password = document.getElementById("password").value;
 
@@ -63,9 +61,7 @@ function validateForm(event) {
 function validateM2F(event) {
   event.preventDefault();
 
-  const codigo = document.getElementById(
-    "code-authn",
-  ).value;
+  const codigo = document.getElementById("code-authn").value;
 
   if (!codigo || !numeroMecanografico || numeroMecanografico == 0) {
     alert("Preenche todos os campos!");
@@ -89,9 +85,48 @@ function validateM2F(event) {
       if (res.status === 200) {
         document.getElementById("login-campo-auth").style.display = "block";
         document.getElementById("login-campo-m2f").style.display = "none";
-        window.location.replace(
-          "http://localhost:8080/pages/admin/adminDashboard.html",
-        );
+        fetch("http://localhost:8080/api/users/me", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then(async (res) => {
+            if (res.status === 200) {
+              const data = await res.json();
+
+              console.log(data.data.role);
+
+              switch (data.data.role) {
+                case "ADMIN":
+                  window.location.replace(
+                    "http://localhost:8080/pages/admin/adminDashboard.html",
+                  );
+                  break;
+                case "MEDICO":
+                  window.location.replace(
+                    "http://localhost:8080/pages/medico/medicoDashboard.html",
+                  );
+                  break;
+                case "ENFERMEIRO":
+                  window.location.replace(
+                    "http://localhost:8080/pages/medico/medicoDashboard.html",
+                  );
+                  break;
+                case "ENFERMEIRO_CHEFE":
+                  window.location.replace(
+                    "http://localhost:8080/pages/medico/medicoDashboard.html",
+                  );
+                  break;
+                default:
+                  console.log("Erro, cargo não existe!");
+                  break;
+              }
+            }
+          })
+          .catch((err) => {
+            console.log("Erro na requisição:", err);
+          });
       }
     })
     .catch((err) => {
