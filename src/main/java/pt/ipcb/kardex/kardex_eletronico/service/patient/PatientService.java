e050021586ba98cf16ab1e6f50590de319243d25
1tsDan <2kupwhite@gmail.com>
2026-04-04 14:07:59 +0100

    feat: add business logic to edit patient's file operation

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/patient/PatientService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/patient/PatientService.java
@@ -1,9 +1,11 @@
 package pt.ipcb.kardex.kardex_eletronico.service.patient;
 
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.CreatePatientFileDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.patient.UpdatePacientFileDTO;
 
 public interface PatientService {
 
     void createPatient(CreatePatientFileDTO data);
     long getHospitalizedPatientsCount();
+    void editPatientFile(Long id, UpdatePacientFileDTO data);
 }
