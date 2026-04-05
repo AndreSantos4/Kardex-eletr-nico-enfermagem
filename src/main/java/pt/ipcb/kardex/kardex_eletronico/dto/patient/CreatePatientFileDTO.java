a3ce7adff1cf8da6718270a0a92b711676e04cc8
1tsDan <2kupwhite@gmail.com>
2026-04-05 16:45:41 +0100

    chore: add ctor to fix null values

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/patient/CreatePatientFileDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/patient/CreatePatientFileDTO.java
@@ -17,12 +17,17 @@ public record CreatePatientFileDTO(
     @JsonProperty("numeroSNS") long numeroSNS,
     @JsonProperty("diagnosticoPrincipal")  String diagnosticoPrincipal,
     @JsonProperty("contacto") int contacto,
-        @JsonProperty("contactoEmergencia") int contactoEmergencia,
+    @JsonProperty("contactoEmergencia") int contactoEmergencia,
     @JsonProperty("medicoId") Long medicoId,
     @JsonProperty("camaId") String camaId,
-    @JsonProperty("alergiasId") List<AlergiaDTO> alergias,
+    @JsonProperty("alergias") List<CreateAlergyDTO> alergias,
     @JsonProperty("motivoInternamento") String motivoInternamento,
     @JsonProperty("flagsRisco") List<FlagRisco> flagsRisco
 ) {
 
+    public CreatePatientFileDTO {
+        if (camaId == null) camaId = "N/A";
+        if (alergias == null) alergias = List.of();
+        if (flagsRisco == null) flagsRisco = List.of();
+    }
 }
