1e433a5fda31eb80075ef86048c868054b0a90f1
1tsDan <2kupwhite@gmail.com>
2026-04-07 23:12:27 +0100

    feat: add endpoint to get all patients

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/PatientController.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/PatientController.java
@@ -1,17 +1,23 @@
 package pt.ipcb.kardex.kardex_eletronico.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.controller.config.ApiResponse;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.CreatePatientFileDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.UpdatePacientFileDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.patient.UtenteDTO;
 import pt.ipcb.kardex.kardex_eletronico.service.patient.PatientService;
 
 @RestController
@@ -31,5 +37,11 @@ public class PatientController {
     public ResponseEntity<ApiResponse<?>> editPatientFile(@PathVariable("patientId") Long id, @RequestBody UpdatePacientFileDTO data){
         service.editPatientFile(id, data);
         return ResponseEntity.ok(ApiResponse.ok("Ficha de utente editada com sucesso", null));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<UtenteDTO>>> getAllPatitents(@RequestParam("f") Optional<String> filter){
+        var patients = service.getAllPatients(filter);
+        return ResponseEntity.ok(ApiResponse.ok("Utentes obtidos com sucesso", patients));
     }
 }
