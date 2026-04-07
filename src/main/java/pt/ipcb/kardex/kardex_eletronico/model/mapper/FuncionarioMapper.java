f1c3ac08c0db30eb84d1f670fe99c51776ee109d
1tsDan <2kupwhite@gmail.com>
2026-04-07 23:13:49 +0100

    refac: remmoved unncessary mapping

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/FuncionarioMapper.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/FuncionarioMapper.java
@@ -3,17 +3,15 @@ package pt.ipcb.kardex.kardex_eletronico.model.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import pt.ipcb.kardex.kardex_eletronico.dto.worker.FuncionarioDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Funcionario;
 
 @Mapper(componentModel = "spring")
 public interface FuncionarioMapper {
-    @Mapping(source = "utilizador", target = "dados")
-    FuncionarioDTO toDTO(Funcionario prescricao);
+    FuncionarioDTO toDTO(Funcionario data);
 
     Funcionario toEntity(FuncionarioDTO dto);
 
-    List<FuncionarioDTO> toDTOList(List<Funcionario> prescricoes);
+    List<FuncionarioDTO> toDTOList(List<Funcionario> data);
 }
