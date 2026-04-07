730ed7f9a5d3c61f0eea873f6a6e6b00fb40d6bc
1tsDan <2kupwhite@gmail.com>
2026-04-07 23:12:50 +0100

    chore: changed fields

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/patient/UtenteDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/patient/UtenteDTO.java
@@ -1,10 +1,7 @@
 package pt.ipcb.kardex.kardex_eletronico.dto.patient;
 
-import pt.ipcb.kardex.kardex_eletronico.model.enumerated.EstadoUtente;
-import pt.ipcb.kardex.kardex_eletronico.model.enumerated.FlagRisco;
+import pt.ipcb.kardex.kardex_eletronico.dto.process.ProcessoClinicoDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.enumerated.Sexo;
-
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -12,14 +9,13 @@ public record UtenteDTO(
     @JsonProperty("id") Long id,
     @JsonProperty("numeroSNS") int numeroSNS,
     @JsonProperty("numeroCC") String numeroCC,
-    @JsonProperty("primerioNome") String primeiroNome,
-    @JsonProperty("apelido") String apelido,
+    @JsonProperty("nome") String nome,
     @JsonProperty("sexo") Sexo sexo,
     @JsonProperty("contacto") int contacto,
     @JsonProperty("contactoEmergencia") int contactoEmergencia,
-    @JsonProperty("estado") EstadoUtente estado,
-    @JsonProperty("alergias") List<AlergiaDTO> alergias,
-    @JsonProperty("flags") List<FlagRisco> flagsRiscos
+    @JsonProperty("temAlergias") Boolean temAlergias,
+    @JsonProperty("temFlags") Boolean temFlags,
+    @JsonProperty("processo") ProcessoClinicoDTO processo
 ) {
 
 }
