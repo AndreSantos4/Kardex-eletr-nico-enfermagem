69c1f1a723e3f4ab50f2873b6646f326ade0e173
1tsDan <2kupwhite@gmail.com>
2026-04-07 23:14:14 +0100

    chore: add mapping rule

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/UtenteMapper.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/UtenteMapper.java
@@ -3,17 +3,20 @@ package pt.ipcb.kardex.kardex_eletronico.model.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.AlergiaDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.CreateAlergyDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.UtenteDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.process.ProcessoClinicoDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Alergia;
-import pt.ipcb.kardex.kardex_eletronico.model.entity.Prescricao;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Utente;
 
 @Mapper(componentModel = "spring")
 public interface UtenteMapper {
-    UtenteDTO toDTO(Prescricao utilizador);
+    @Mapping(target = "id", source = "utente.id")
+    @Mapping(target = "processo", source = "processoClinico")
+    UtenteDTO toDto(Utente utente, ProcessoClinicoDTO processoClinico);
 
     Utente toEntity(UtenteDTO dto);
 
