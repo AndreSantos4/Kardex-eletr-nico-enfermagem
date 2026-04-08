2efb2f8c999c8dd36bdc1481adfe31a5ac3487c3
AndreSantos4 <149972193+AndreSantos4@users.noreply.github.com>
2026-04-08 14:37:19 +0100

    fix: correct file paths for user redirection after login

--- src/main/resources/static/scripts/logout.js
+++ src/main/resources/static/scripts/logout.js
@@ -1,20 +1,17 @@
 async function logout() {
-    try {
-        const response = await fetch(
-            `http://localhost:8080/api/auth/logout`,
-            {
-                method: "POST",
-                credentials: "include",
-                headers: { "Content-Type": "application/json" },
-            }
-        );
+  try {
+    const response = await fetch(`http://localhost:8080/api/auth/logout`, {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            window.location.replace("http://localhost:8080/pages/login/login.html");
-        } else {
-            console.error("Erro ao fechar sessão!")
-        }
-    } catch (_) {
-        console.error("Erro no servidor!")
+    if (response.ok) {
+      window.location.replace("http://localhost:8080/login");
+    } else {
+      console.error("Erro ao fechar sessão!");
     }
+  } catch (_) {
+    console.error("Erro no servidor!");
+  }
 }
