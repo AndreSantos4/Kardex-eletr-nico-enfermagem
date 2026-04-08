89d9ac079f0db9f653102b38244ca59bf0d7f6cb
1tsDan <2kupwhite@gmail.com>
2026-04-08 18:59:58 +0100

    feat: add find by estado query

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/UtenteRepository.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/UtenteRepository.java
@@ -1,8 +1,11 @@
 package pt.ipcb.kardex.kardex_eletronico.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import pt.ipcb.kardex.kardex_eletronico.controller.filter.PatientState;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Utente;
 import pt.ipcb.kardex.kardex_eletronico.model.enumerated.EstadoUtente;
 
@@ -10,5 +13,7 @@ import pt.ipcb.kardex.kardex_eletronico.model.enumerated.EstadoUtente;
 public interface UtenteRepository extends JpaRepository<Utente, Long>{
 
     long countByEstado(EstadoUtente internado);
+
+    List<Utente> findByEstado(EstadoUtente parsedFilter);
 
 }
