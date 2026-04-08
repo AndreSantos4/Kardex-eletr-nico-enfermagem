cf4bb4515b3a51a143f862e732d655febdfe2b7e
1tsDan <2kupwhite@gmail.com>
2026-04-08 09:17:09 +0100

    feat: add get all beds endpoint

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/ProcessController.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/ProcessController.java
@@ -1,10 +1,14 @@
 package pt.ipcb.kardex.kardex_eletronico.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.controller.config.ApiResponse;
 import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreateAdministrationDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreatePrescriptionDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.process.CamaDTO;
 import pt.ipcb.kardex.kardex_eletronico.service.process.ProcessService;
 
 @RestController
@@ -34,5 +39,11 @@ public class ProcessController {
 
         service.administrateMedication(prescriptionId, data, request);
         return ResponseEntity.ok(ApiResponse.ok("Administracao efetuada com sucesso", null));
+    }
+
+    @GetMapping("/beds")
+    public ResponseEntity<ApiResponse<List<CamaDTO>>> getAllBeds(@RequestParam(name = "o", defaultValue = "false") boolean occupied){
+        var beds = service.getAllBeds(occupied);
+        return ResponseEntity.ok(ApiResponse.ok("Camas obtidas com sucesso", beds));
     }
 }
