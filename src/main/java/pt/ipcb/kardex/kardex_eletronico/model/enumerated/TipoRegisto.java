1df6a6650aec161720dfeec2705e42cabe267afe
1tsDan <2kupwhite@gmail.com>
2026-04-14 18:19:34 +0100

    chore: add enum values

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/enumerated/TipoRegisto.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/enumerated/TipoRegisto.java
@@ -3,7 +3,9 @@ package pt.ipcb.kardex.kardex_eletronico.model.enumerated;
 public enum TipoRegisto {
     AUTH("Autenticação"),
     PATIENT_ACCEPTANCE("Utente admitido"),
-    PATIENT_DISCHARGE("Utente liberado");
+    PATIENT_DISCHARGE("Utente liberado"),
+    USER_CREATION("Criacao de utilizador"),
+    PASSWORD_RESET_REQUEST("Pedido de alteracao de password");
     
     public final String nome;
     
