73ee6b4fea1d1914eb6c6f44a37769de63c1f7ba
1tsDan <2kupwhite@gmail.com>
2026-04-28 11:16:58 +0100

    chore: change from time interval to fixed number

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/FrequenciaDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/FrequenciaDTO.java
@@ -7,8 +7,7 @@ import pt.ipcb.kardex.kardex_eletronico.model.enumerated.Periodo;
 public record FrequenciaDTO (
     @JsonProperty("frequencia") int frequencia,
     @JsonProperty("periodo") Periodo periodo,
-    @JsonProperty("horaInferior") int horaInferior,
-    @JsonProperty("horaSuperior") int horaSuperior
+    @JsonProperty("intervaloMinHoras") int intervaloMinHoras
 ) {
     
 }
