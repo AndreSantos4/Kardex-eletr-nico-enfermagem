ef77f566fd868d2534d487c985762ab722c299fb
1tsDan <2kupwhite@gmail.com>
2026-04-29 22:29:32 +0100

    chore: add date field

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/CreateAdministrationDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/CreateAdministrationDTO.java
@@ -1,10 +1,14 @@
 package pt.ipcb.kardex.kardex_eletronico.dto.prescription;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CreateAdministrationDTO(
     @JsonProperty("observacoes") String observacoes,
-    @JsonProperty("foi_administrado") Boolean administrado
+    @JsonProperty("foi_administrado") Boolean administrado,
+    @JsonProperty("data") @JsonFormat(pattern = "dd/MM/yyyy:HH:mm:ss") LocalDateTime data
 ) {
 
 }
