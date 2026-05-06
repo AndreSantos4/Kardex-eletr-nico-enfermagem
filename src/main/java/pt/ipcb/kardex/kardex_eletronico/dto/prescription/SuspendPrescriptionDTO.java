14d3fdaa5bcccb1f73b6a2660140ea57ee44b3cc
1tsDan <2kupwhite@gmail.com>
2026-05-06 21:35:27 +0100

    feat: create dto to hold prescription suspension data

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/SuspendPrescriptionDTO.java
@@ -0,0 +1,11 @@
+package pt.ipcb.kardex.kardex_eletronico.dto.prescription;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SuspendPrescriptionDTO(
+    @JsonProperty("definitiva") boolean definitiva,
+    @JsonProperty("dataRetorno") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataRetorno
+){}
