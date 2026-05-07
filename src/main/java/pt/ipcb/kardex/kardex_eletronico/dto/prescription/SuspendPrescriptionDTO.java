d79c8f9feb782d557b5f262ebc67122dead18b48
1tsDan <2kupwhite@gmail.com>
2026-05-07 20:08:07 +0100

    chore: add new fields

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/SuspendPrescriptionDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/SuspendPrescriptionDTO.java
@@ -5,7 +5,11 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import pt.ipcb.kardex.kardex_eletronico.model.enumerated.MotivoClinico;
+
 public record SuspendPrescriptionDTO(
     @JsonProperty("definitiva") boolean definitiva,
-    @JsonProperty("dataRetorno") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataRetorno
+    @JsonProperty("dataRetorno") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataRetorno,
+    @JsonProperty("motivo") MotivoClinico motivo,
+    @JsonProperty("observacoes") String observacoes
 ){}
