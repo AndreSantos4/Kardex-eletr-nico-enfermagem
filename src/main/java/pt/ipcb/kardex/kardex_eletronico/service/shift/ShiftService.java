e25bf9d294f16ddb62cf372216795bca8bc66d29
1tsDan <2kupwhite@gmail.com>
2026-05-09 18:14:49 +0100

    feat: add business logic for shift managment and nurse assignment

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/shift/ShiftService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/shift/ShiftService.java
@@ -1,8 +1,15 @@
 package pt.ipcb.kardex.kardex_eletronico.service.shift;
 
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.AssignNursesDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
 
 public interface ShiftService {
     
-    void CreateShift(CreateShiftDTO data);
+    void createShift(CreateShiftDTO data);
+
+    void editShift(Long shiftId, CreateShiftDTO data);
+
+    void deleteShift(Long shiftId);
+
+    void assignNurses(Long shiftId, AssignNursesDTO data);
 }
--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/shift/ShiftServiceImpl.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/shift/ShiftServiceImpl.java
@@ -1,5 +1,6 @@
 package pt.ipcb.kardex.kardex_eletronico.service.shift;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.HashSet;
@@ -8,36 +9,97 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.AssignNursesDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
+import pt.ipcb.kardex.kardex_eletronico.exception.ConflictEntitiesException;
+import pt.ipcb.kardex.kardex_eletronico.exception.EntityNotFoundException;
 import pt.ipcb.kardex.kardex_eletronico.exception.KardexException;
+import pt.ipcb.kardex.kardex_eletronico.model.entity.AtribuicaoUtente;
+import pt.ipcb.kardex.kardex_eletronico.model.entity.Turno;
 import pt.ipcb.kardex.kardex_eletronico.model.enumerated.Role;
 import pt.ipcb.kardex.kardex_eletronico.model.enumerated.TipoTurno;
 import pt.ipcb.kardex.kardex_eletronico.model.mapper.TurnoMapper;
 import pt.ipcb.kardex.kardex_eletronico.repository.TurnoRepository;
+import pt.ipcb.kardex.kardex_eletronico.service.patient.PatientService;
 import pt.ipcb.kardex.kardex_eletronico.service.worker.WorkerService;
 
 @Service
 @RequiredArgsConstructor
 public class ShiftServiceImpl implements ShiftService{
 
+    private final Clock clock;
+
     private final TurnoRepository repository;
     private final TurnoMapper mapper;
     private final WorkerService workerService;
+    private final PatientService patientService;
 
     @Override
     @Transactional
-    public void CreateShift(CreateShiftDTO data) {
+    public void createShift(CreateShiftDTO data) {
         var shift = mapper.fromCreate(data);
+        validateShift(data, shift);
+        setAlocatedNurses(data, shift);
 
+        repository.save(shift);
+    }
+
+    @Override
+    @Transactional
+    public void editShift(Long shiftId, CreateShiftDTO data) {
+        var shift = getValidShift(shiftId);
+
+        if (shift.getInicio().isBefore(LocalDateTime.now(clock))) {
+            throw new KardexException("Nao e possivel editar um turno que ja foi iniciado");
+        }
+
+        shift.setTipo(data.tipo());
+
+        validateShift(data, shift);
+        setAlocatedNurses(data, shift);
+    }
+
+    private void setAlocatedNurses(CreateShiftDTO data, Turno shift) {
+        shift.getEnfermeiros().forEach(worker -> worker.getTurnos().remove(shift));
+        shift.getEnfermeiros().clear();
+
+        var workers = data.IdEnfermeiros()
+                .stream()
+                .map(id -> {
+                    var worker = workerService.getWorker(id);
+                    if(worker.getDados().getRole() != Role.ENFERMEIRO){
+                        throw new KardexException("So podem ser alocados enfermeiros a um turno");
+                    }
+                    if(!workerService.isAvailable(worker, shift.inicio, shift.fim)){
+                        throw new ConflictEntitiesException("Funcionario de id " + worker.getId() + " com turnos sobrepostos");
+                    }
+
+                    worker.getTurnos().add(shift);
+
+                    return worker;
+                })
+                .toList();
+
+        shift.setEnfermeiros(new HashSet<>(workers));
+    }
+
+    private void validateShift(CreateShiftDTO data, Turno shift) {
         switch (data.tipo()) {
             case TipoTurno.CUSTOM:
-                if(shift.getInicio() == null || shift.getFim() == null){
-                    throw new KardexException("Para turnos com horario personalizado, e necessario especifar a hora de inicio e de fim");
+                if (data.horaInicio() == null || data.horaFim() == null) {
+                    throw new KardexException(
+                            "Para turnos com horario personalizado, e necessario especificar a hora de inicio e de fim"
+                    );
                 }
-                if(shift.getInicio().isAfter(shift.getFim())){
-                    throw new KardexException("A hora de fim nao pode ser inferior a hora de inicio");
+                shift.setInicio(LocalDateTime.of(data.data(), data.horaInicio()));
+                if (data.horaInicio().isAfter(data.horaFim())) {
+                    shift.setFim(LocalDateTime.of(data.data().plusDays(1), data.horaFim()));
+                } else if (data.horaInicio().equals(data.horaFim())){
+                    throw new KardexException("Hora de fim e inicio nao podem ser iguais");
+                } else {
+                    shift.setFim(LocalDateTime.of(data.data(), data.horaFim()));
                 }
-            break;
+                break;
             case TipoTurno.MANHA:
                 shift.setInicio(LocalDateTime.of(data.data(), LocalTime.of(8, 0)));
                 shift.setFim(LocalDateTime.of(data.data(), LocalTime.of(16, 0)));
@@ -51,22 +113,46 @@ public class ShiftServiceImpl implements ShiftService{
                 shift.setFim(LocalDateTime.of(data.data(), LocalTime.of(8, 0)));
                 break;
         }
+    }
 
-        var workers = data.IdEnfermeiros()
-            .stream()
-            .map(id -> {
-            var worker = workerService.getWorker(id);
+    @Override
+    @Transactional
+    public void deleteShift(Long shiftId) {
+        var shift = getValidShift(shiftId);
+
+        if(!shift.getEnfermeiros().isEmpty()){
+            throw new KardexException("Nao e possivel eliminar um turno com enfermeiros alocados");
+        }
+
+        repository.delete(shift);
+    }
+
+    @Override
+    @Transactional
+    public void assignNurses(Long shiftId, AssignNursesDTO data) {
+        var shift = getValidShift(shiftId);
+
+        data.atribuicoes().forEach(a -> {
+            var patient = patientService.getValidPatient(a.idUtente());
+            var worker = workerService.getWorker(a.idEnfermeiro());
+
             if(worker.getDados().getRole() != Role.ENFERMEIRO){
-                throw new KardexException("So podem ser alocados enfermeiros a um turno");
+                throw new KardexException("O funcionario associado nao e um enfermeiro");
             }
-            worker.getTurnos().add(shift);
 
-            return worker;
-        })
-        .toList();
+            var assignment = new AtribuicaoUtente(null, worker, patient, shift);
+            shift.getAtribuicoes().add(assignment);
+        });
+    }
 
-        shift.setEnfermeiros(new HashSet<>(workers));
+    private Turno getValidShift(Long shiftId){
+        var shift = repository.findById(shiftId)
+                .orElseThrow(() -> EntityNotFoundException.forId(shiftId, "Turno"));
 
-        repository.save(shift);
+        if(shift.getFim().isBefore(LocalDateTime.now(clock))){
+            throw new KardexException("Nao e possivel alterar um turno passado");
+        }
+
+        return shift;
     }
 }
