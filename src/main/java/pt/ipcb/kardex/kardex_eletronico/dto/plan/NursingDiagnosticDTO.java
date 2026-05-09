836ab81c1bebe61463ff45d9bc61ec8cd8c25c8d
1tsDan <2kupwhite@gmail.com>
2026-05-09 21:46:19 +0100

    refac: change from data holder dto to data creation dto

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/plan/NursingDiagnosticDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/plan/NursingDiagnosticDTO.java
@@ -1,11 +1,16 @@
 package pt.ipcb.kardex.kardex_eletronico.dto.plan;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import pt.ipcb.kardex.kardex_eletronico.model.enumerated.PrioridadeDiagnostico;
 
+import java.time.LocalDate;
+
 public record NursingDiagnosticDTO(
-    @JsonProperty("id") Long id,
-    @JsonProperty("diagnostico") String diagnostico,
-    @JsonProperty("prioridade") PrioridadeDiagnostico prioridade
-){}
+        @JsonProperty("id") Long id,
+        @JsonProperty("diagnostico") String diagnostico,
+        @JsonProperty("prioridade") PrioridadeDiagnostico prioridade,
+        @JsonProperty("dataCriacao") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataCriacao
+){
+}
