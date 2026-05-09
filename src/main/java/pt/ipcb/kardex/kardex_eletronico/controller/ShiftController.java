e2fcadcae189a477e7f91e02545a488d140928d1
1tsDan <2kupwhite@gmail.com>
2026-05-09 18:14:35 +0100

    feat: add endpoints for shift managment and nurse assignment

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/ShiftController.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/ShiftController.java
@@ -1,13 +1,11 @@
 package pt.ipcb.kardex.kardex_eletronico.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.controller.config.ApiResponse;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.AssignNursesDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
 import pt.ipcb.kardex.kardex_eletronico.service.shift.ShiftService;
 
@@ -20,7 +18,25 @@ public class ShiftController{
 
     @PostMapping
     public ResponseEntity<ApiResponse<?>> createShift(@RequestBody CreateShiftDTO data){
-        service.CreateShift(data);
+        service.createShift(data);
         return ResponseEntity.ok(ApiResponse.ok("Turno criado com sucesso", null));
+    }
+
+    @PatchMapping("/{shiftId}")
+    public ResponseEntity<ApiResponse<?>> editShift(@PathVariable Long shiftId, @RequestBody CreateShiftDTO data){
+        service.editShift(shiftId, data);
+        return ResponseEntity.ok(ApiResponse.ok("Turno editado com sucesso", null));
+    }
+
+    @DeleteMapping("/{shiftId}")
+    public ResponseEntity<ApiResponse<?>> deleteShift(@PathVariable Long shiftId){
+        service.deleteShift(shiftId);
+        return ResponseEntity.ok(ApiResponse.ok("Turno eliminado com sucesso", null));
+    }
+
+    @PostMapping("/{shiftId}/assignments")
+    public ResponseEntity<ApiResponse<?>> assignNurse(@PathVariable Long shiftId, @RequestBody AssignNursesDTO data){
+        service.assignNurses(shiftId, data);
+        return ResponseEntity.ok(ApiResponse.ok("Enfermeiros atribuidos com sucesso", null));
     }
 }
