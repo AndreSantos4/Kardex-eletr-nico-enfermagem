2c8e718c41ab57e7637ce0c149bd8e0073f6637c
1tsDan <2kupwhite@gmail.com>
2026-05-10 23:24:29 +0100

    feat: add enum to store intervencao entity frequency state

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/enumerated/FrequenciaIntervencao.java
@@ -0,0 +1,14 @@
+package pt.ipcb.kardex.kardex_eletronico.model.enumerated;
+
+public enum FrequenciaIntervencao {
+    DIA_1,
+    DIA_2,
+    DIA_3,
+    DIA_4,
+    DIA_5,
+    DIA_6,
+    H_2,
+    H_4,
+    CONTINUA,
+    SOS
+}
