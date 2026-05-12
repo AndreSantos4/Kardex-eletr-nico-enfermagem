e1983c4ed0078356bafad8f591118cd2e145c3d7
1tsDan <2kupwhite@gmail.com>
2026-05-12 20:42:33 +0100

    feat: create a default  care plan for every new process

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/process/ProcessServiceImpl.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/process/ProcessServiceImpl.java
@@ -1,28 +1,16 @@
 package pt.ipcb.kardex.kardex_eletronico.service.process;
 
-import java.time.LocalDate;
+import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.CateterDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.ContencaoDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.CreateCateterDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.CreateContencaoDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.CreateIncidenteDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.IncidenteDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.RegisterVitalSignsDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.UpdatePacientFileDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.plan.CreateCarePlanDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.plan.CreateInterventionDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.plan.PlanoCuidadosDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreateAdministrationDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.prescription.CreatePrescriptionDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.prescription.PrescricaoDTO;
-import pt.ipcb.kardex.kardex_eletronico.dto.prescription.SuspendPrescriptionDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.process.CamaDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.process.CreateProcessDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.process.DischargePatientDTO;
@@ -31,29 +19,21 @@ import pt.ipcb.kardex.kardex_eletronico.exception.ConflictEntitiesException;
 import pt.ipcb.kardex.kardex_eletronico.exception.EntityNotFoundException;
 import pt.ipcb.kardex.kardex_eletronico.exception.InactiveResourceException;
 import pt.ipcb.kardex.kardex_eletronico.exception.KardexException;
-import pt.ipcb.kardex.kardex_eletronico.model.entity.AdministracaoMedicacao;
-import pt.ipcb.kardex.kardex_eletronico.model.entity.Dosagem;
-import pt.ipcb.kardex.kardex_eletronico.model.entity.Medicamento;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.ProcessoClinico;
-import pt.ipcb.kardex.kardex_eletronico.model.entity.SuspensaoClinica;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Utente;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.*;
-import pt.ipcb.kardex.kardex_eletronico.model.enumerated.EstadoUtente;
-import pt.ipcb.kardex.kardex_eletronico.model.enumerated.Periodo;
-import pt.ipcb.kardex.kardex_eletronico.model.enumerated.PrescriptionState;
+import pt.ipcb.kardex.kardex_eletronico.model.enumerated.*;
 import pt.ipcb.kardex.kardex_eletronico.model.mapper.*;
-import pt.ipcb.kardex.kardex_eletronico.repository.AdministracaoRepository;
 import pt.ipcb.kardex.kardex_eletronico.repository.CamaRepository;
-import pt.ipcb.kardex.kardex_eletronico.repository.PlanoRepository;
-import pt.ipcb.kardex.kardex_eletronico.repository.PrescricaoRepository;
 import pt.ipcb.kardex.kardex_eletronico.repository.ProcessoClinicoRepository;
 import pt.ipcb.kardex.kardex_eletronico.service.record.RecordService;
-import pt.ipcb.kardex.kardex_eletronico.service.stock.StockService;
 import pt.ipcb.kardex.kardex_eletronico.service.worker.WorkerService;
 
 @Service
 @RequiredArgsConstructor
 public class ProcessServiceImpl implements ProcessService{
+
+    private final Clock clock;
 
     private final ProcessoClinicoRepository repository;
     private final ProcessoMapper mapper;
@@ -71,11 +51,14 @@ public class ProcessServiceImpl implements ProcessService{
         var process = mapper.fromCreate(data);
         var medic = workerService.getMedicById(data.medicoId());
         var bed = camaRepository.findById(data.camaId()).orElse(null);
+        var carePlan = defaultCarePlan();
 
         process.setMedicoResponsavel(medic);
         process.setUtente(patient);
         process.setCama(bed);
+        process.getPlanoCuidados().add(carePlan);
         patient.setEstado(EstadoUtente.INTERNADO);
+        carePlan.setProcessoClinico(process);
 
         if(bed == null) {
             throw new KardexException("Cama nao pode ser nula");
@@ -84,6 +67,66 @@ public class ProcessServiceImpl implements ProcessService{
         bed.setOcupada(true);
 
         return mapper.toDTO(repository.save(process));
+    }
+
+    private PlanoCuidados defaultCarePlan() {
+        var plan =  new PlanoCuidados();
+        var autenticatedWorker = workerService.getAutenticatedWorker();
+
+        var interventions = defaultInterventions();
+        interventions.forEach(intervention -> intervention.setPlanoCuidados(plan));
+        plan.setAutor(autenticatedWorker);
+        plan.setIntervencoes(interventions);
+
+        return plan;
+    }
+
+    private List<Intervencao>  defaultInterventions() {
+        List<Intervencao> intervencoes = new ArrayList<>();
+        var autenticatedWorker = workerService.getAutenticatedWorker();
+
+        Intervencao intervencao1 = new Intervencao();
+        intervencao1.setFuncionario(autenticatedWorker);
+        intervencao1.setDiagnostico(IntervencaoDiagnostico.MONITORIZACAO_SINAIS_VITAIS);
+        intervencao1.setIntervencao(TipoIntervencao.OUTRO);
+        intervencao1.setFrequencia(FrequenciaIntervencao.DIA_4);
+        intervencao1.setHorarioPrevisto("06:00 - 12:00 - 18:00 - 24:00");
+        intervencao1.setPrioridade(PrioridadeIntervencao.MEDIA);
+        intervencao1.setData(LocalDateTime.now(clock));
+
+        Intervencao intervencao2 = new Intervencao();
+        intervencao2.setFuncionario(autenticatedWorker);
+        intervencao2.setDiagnostico(IntervencaoDiagnostico.AVALIACAO_NIVEL_DOR);
+        intervencao2.setIntervencao(TipoIntervencao.OUTRO);
+        intervencao2.setFrequencia(FrequenciaIntervencao.DIA_3);
+        intervencao2.setHorarioPrevisto("08:00 - 20:00");
+        intervencao2.setPrioridade(PrioridadeIntervencao.MEDIA);
+        intervencao2.setData(LocalDateTime.now(clock));
+
+        Intervencao intervencao3 = new Intervencao();
+        intervencao3.setFuncionario(autenticatedWorker);
+        intervencao3.setDiagnostico(IntervencaoDiagnostico.SUPERVISAO_CONTINUA_UTENTE);
+        intervencao3.setIntervencao(TipoIntervencao.OUTRO);
+        intervencao3.setFrequencia(FrequenciaIntervencao.CONTINUA);
+        intervencao3.setHorarioPrevisto("Sempre");
+        intervencao3.setPrioridade(PrioridadeIntervencao.MEDIA);
+        intervencao3.setData(LocalDateTime.now(clock));
+
+        Intervencao intervencao4 = new Intervencao();
+        intervencao4.setFuncionario(autenticatedWorker);
+        intervencao4.setDiagnostico(IntervencaoDiagnostico.GLICEMIA_CAPILAR_PRE_JANTAR);
+        intervencao4.setIntervencao(TipoIntervencao.OUTRO);
+        intervencao4.setFrequencia(FrequenciaIntervencao.DIA_1);
+        intervencao4.setHorarioPrevisto("18:30");
+        intervencao4.setPrioridade(PrioridadeIntervencao.MEDIA);
+        intervencao4.setData(LocalDateTime.now(clock));
+
+        intervencoes.add(intervencao1);
+        intervencoes.add(intervencao2);
+        intervencoes.add(intervencao3);
+        intervencoes.add(intervencao4);
+
+        return intervencoes;
     }
 
     @Override
