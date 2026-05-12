60ab74ed3834aeb0e0aa61821a5ec9bfb555e903
1tsDan <2kupwhite@gmail.com>
2026-05-12 12:05:47 +0100

    fix: change from list of ids to list of workers

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/TurnoDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/TurnoDTO.java
@@ -6,15 +6,15 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import pt.ipcb.kardex.kardex_eletronico.dto.worker.FuncionarioDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.enumerated.TipoTurno;
-import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.IncidenteDTO;
 
 public record TurnoDTO(
     @JsonProperty("id") Long id,
     @JsonProperty("tipo") TipoTurno tipo,
     @JsonProperty("inicio") @JsonFormat(pattern = "dd/MM/yyyy:HH:mm:ss")  LocalDateTime inicio,
     @JsonProperty("fim") @JsonFormat(pattern = "dd/MM/yyyy:HH:mm:ss")  LocalDateTime fim,
-    @JsonProperty("IdEnfermeiros") List<Long> IdEnfermeiros,
+    @JsonProperty("IdEnfermeiros") List<FuncionarioDTO> enfermeiros,
     @JsonProperty("observacoes") String observacoes
 ) {
     
