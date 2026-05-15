da524e494ed431ed62875d9101db6c7d1a142a00
1tsDan <2kupwhite@gmail.com>
2026-05-15 17:39:12 +0100

    feat: get next shift query

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/TurnoRepository.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/TurnoRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Turno;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface TurnoRepository extends JpaRepository<Turno, Long>{
-
+    Optional<Turno> findFirstByInicioAfterOrderByInicioDesc(LocalDateTime now);
 }
