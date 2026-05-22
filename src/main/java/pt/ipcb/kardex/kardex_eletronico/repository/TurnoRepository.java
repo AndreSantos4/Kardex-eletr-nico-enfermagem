d6bced055875d2b10b36e44a27f5a3bb38bd8b2a
1tsDan <2kupwhite@gmail.com>
2026-05-22 11:58:09 +0100

    feat: add queries

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/TurnoRepository.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/repository/TurnoRepository.java
@@ -1,26 +1,49 @@
 package pt.ipcb.kardex.kardex_eletronico.repository;
 
+import java.security.cert.PKIXRevocationChecker.Option;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Turno;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface TurnoRepository extends JpaRepository<Turno, Long>{
+public interface TurnoRepository extends JpaRepository<Turno, Long> {
+    @Query(
+        "SELECT t FROM Turno t LEFT JOIN t.passagemTurno pt WHERE (pt IS NULL OR pt.ativo = true) AND t.fim < :now"
+    )
+    List<Turno> findAllWithoutPassagemTurnoBeforeNow(
+        @Param("now") LocalDateTime now
+    );
 
-    @Query("SELECT t FROM Turno t LEFT JOIN t.passagemTurno pt WHERE (pt IS NULL OR pt.ativo = true) AND t.fim < :now")
-    List<Turno> findAllWithoutPassagemTurnoBeforeNow(@Param("now") LocalDateTime now);
-
-    Optional<Turno> findFirstByPassagemTurnoPendenteTrueAndEnfermeiros_IdOrderByInicioDesc(Long workerId);
+    Optional<
+        Turno
+    > findFirstByPassagemTurnoPendenteTrueAndEnfermeiros_IdOrderByInicioDesc(
+        Long workerId
+    );
 
     Optional<Turno> findFirstByFimAfterOrderByInicioAsc(LocalDateTime now);
 
-    @Query("SELECT t FROM Turno t JOIN t.enfermeiros f WHERE f.id = :id AND t.inicio <= :now AND t.fim >= :now")
-    Optional<Turno> findTurnoAtivoByFuncionarioId(@Param("id") Long id, @Param("now") LocalDateTime now);
+    @Query(
+        "SELECT t FROM Turno t JOIN t.enfermeiros f WHERE f.id = :id AND t.inicio <= :now AND t.fim >= :now"
+    )
+    Optional<Turno> findTurnoAtivoByFuncionarioId(
+        @Param("id") Long id,
+        @Param("now") LocalDateTime now
+    );
+
+    @Query("SELECT t FROM Turno t WHERE t.inicio < :now AND t.fim > :now")
+    Optional<Turno> findCurrentShift(@Param("now") LocalDateTime now);
+
+    @Query(
+        "SELECT COUNT(t) > 0 FROM Turno t WHERE t.inicio < :end AND t.fim > :start AND t.id <> :excludeId"
+    )
+    boolean existsOverlap(
+        @Param("start") LocalDateTime start,
+        @Param("end") LocalDateTime end,
+        @Param("excludeId") Long excludeId
+    );
 }
