2c51406089276825c56ec725e70357bb328fecb8
1tsDan <2kupwhite@gmail.com>
2026-05-23 04:46:24 +0100

    feat: add get patient history business logic

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/service/patient/PatientService.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/service/patient/PatientService.java
@@ -10,6 +10,8 @@ import pt.ipcb.kardex.kardex_eletronico.dto.patient.CreatePatientFileDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.PatientKardexDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.UpdatePacientFileDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.UtenteDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.process.LimitedProcessoClinicoDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.process.ProcessoClinicoDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Utente;
 
 public interface PatientService {
@@ -21,4 +23,6 @@ public interface PatientService {
     List<UtenteDTO> getAllPatients(PatientState filter2, Optional<String> search);
     List<AlergiaDTO> getAllAlergies();
     PatientKardexDTO getPatientKardex(Long patientId);
+
+    List<LimitedProcessoClinicoDTO> getHistory(Long patientId);
 }
