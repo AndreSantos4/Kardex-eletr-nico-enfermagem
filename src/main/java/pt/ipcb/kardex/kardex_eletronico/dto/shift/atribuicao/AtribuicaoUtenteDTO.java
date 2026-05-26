4c02c3b8643e5e712397ab548bbb9256c55295a2
1tsDan <2kupwhite@gmail.com>
2026-05-26 21:51:40 +0100

    refac: move to shift/atribuicao package

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/atribuicao/AtribuicaoUtenteDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/atribuicao/AtribuicaoUtenteDTO.java
@@ -1,7 +1,8 @@
-package pt.ipcb.kardex.kardex_eletronico.dto.shift;
+package pt.ipcb.kardex.kardex_eletronico.dto.shift.atribuicao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import pt.ipcb.kardex.kardex_eletronico.dto.patient.UtenteDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.LimitedTurnoDTO;
 
 public record AtribuicaoUtenteDTO(
         @JsonProperty("utente") UtenteDTO utente,
