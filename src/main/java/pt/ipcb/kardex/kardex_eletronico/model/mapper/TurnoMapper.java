8ccdb1cf6b46f76a4640e4f36ede660435ba7313
1tsDan <2kupwhite@gmail.com>
2026-05-26 21:53:48 +0100

    feat: add mapping for detailed shift change

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/TurnoMapper.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/TurnoMapper.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.mapstruct.Mapper;
 
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.CreateShiftDTO;
+import pt.ipcb.kardex.kardex_eletronico.dto.shift.DetailedShiftChangeDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.LimitedTurnoDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.PendenciaDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.TurnoDTO;
+import pt.ipcb.kardex.kardex_eletronico.model.entity.PassagemTurno;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Pendencia;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Turno;
 
@@ -24,4 +26,6 @@ public interface TurnoMapper {
     List<TurnoDTO> toDTOList(List<Turno> turnos);
 
     List<PendenciaDTO> toIssuesDTOList(List<Pendencia> issues);
+
+    DetailedShiftChangeDTO toDetailsChangeDTO(PassagemTurno data, List<Pendencia> dadosTurnoUtente);
 }
