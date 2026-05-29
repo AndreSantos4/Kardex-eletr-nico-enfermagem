0d612980bf242b797056cfdc7fad4e0c347eefab
1tsDan <2kupwhite@gmail.com>
2026-05-29 05:16:48 +0100

    refac: change package

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/administration/AdministracaoDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/prescription/administration/AdministracaoDTO.java
@@ -1,9 +1,10 @@
-package pt.ipcb.kardex.kardex_eletronico.dto.prescription;
+package pt.ipcb.kardex.kardex_eletronico.dto.prescription.administration;
 
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import pt.ipcb.kardex.kardex_eletronico.dto.prescription.PrescricaoDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.shift.TurnoDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.worker.FuncionarioDTO;
 
