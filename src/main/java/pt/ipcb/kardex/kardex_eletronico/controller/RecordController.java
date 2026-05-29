35314a9f654113c09bb9c783d7012221e5c0a2ac
1tsDan <2kupwhite@gmail.com>
2026-05-29 19:46:05 +0100

    feat: add endpoint to get all clinic records

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/RecordController.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/RecordController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ipcb.kardex.kardex_eletronico.controller.config.ApiResponse;
 import pt.ipcb.kardex.kardex_eletronico.controller.filter.RecordFilter;
+import pt.ipcb.kardex.kardex_eletronico.dto.record.RegistoClinicoDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.record.RegistoDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.util.Pagination;
+import pt.ipcb.kardex.kardex_eletronico.service.record.ClinicRecordService;
 import pt.ipcb.kardex.kardex_eletronico.service.record.RecordService;
 
 import java.util.List;
@@ -19,10 +21,17 @@ import java.util.List;
 public class RecordController {
 
     private final RecordService service;
+    private final ClinicRecordService clinicRecordService;
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<RegistoDTO>>> getRecords(Pagination pagination, RecordFilter filter){
         var records = service.getRecords(pagination, filter);
+        return ResponseEntity.ok(ApiResponse.ok("Registos obtidos com sucesso", records));
+    }
+
+    @GetMapping("/clinic")
+    public ResponseEntity<ApiResponse<List<RegistoClinicoDTO>>> getAllClinicRecords(){
+        var records = clinicRecordService.getAllRecords();
         return ResponseEntity.ok(ApiResponse.ok("Registos obtidos com sucesso", records));
     }
 }
