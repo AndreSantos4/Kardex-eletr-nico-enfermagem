99240240d1f0d3e1c7fd846016a464fdbe0a6e97
Luís Martins <Luis_J_B_Martins@hotmail.com>
2026-05-29 18:13:04 +0100

    chore: added something

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/PassagemTurnoDTO.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/dto/shift/PassagemTurnoDTO.java
@@ -1,13 +1,17 @@
 package pt.ipcb.kardex.kardex_eletronico.dto.shift;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import pt.ipcb.kardex.kardex_eletronico.dto.worker.LimitedFuncionarioDTO;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record PassagemTurnoDTO(
         @JsonProperty("id") Long id,
-        @JsonProperty("turno") LimitedTurnoDTO turno,
+        @JsonProperty("turno") TurnoDTO turno,
         @JsonProperty("proximoTurno") LimitedTurnoDTO proximoTurno,
-        @JsonProperty("dadosTurnoUtentes") List<PendenciaDTO> dadosTurnoUtente
+        @JsonProperty("dadosTurnoUtentes") List<PendenciaDTO> dadosTurnoUtente,
+        @JsonProperty("validador") LimitedFuncionarioDTO validador,
+        @JsonProperty("dataValidacao") LocalDateTime dataValidacao
 ) {
 }
