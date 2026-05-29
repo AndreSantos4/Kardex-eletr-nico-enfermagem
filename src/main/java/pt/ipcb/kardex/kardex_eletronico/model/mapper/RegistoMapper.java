bdc1de8956965a82901e0d3d010062144d2a226b
1tsDan <2kupwhite@gmail.com>
2026-05-29 19:46:27 +0100

    feat: add clinic records mapper methods

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/RegistoMapper.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/mapper/RegistoMapper.java
@@ -2,8 +2,10 @@ package pt.ipcb.kardex.kardex_eletronico.model.mapper;
 
 import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
+import pt.ipcb.kardex.kardex_eletronico.dto.record.RegistoClinicoDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.record.RegistoDTO;
 import pt.ipcb.kardex.kardex_eletronico.model.entity.Registo;
+import pt.ipcb.kardex.kardex_eletronico.model.entity.RegistoClinico;
 
 import java.util.List;
 
@@ -13,4 +15,8 @@ public interface RegistoMapper {
     RegistoDTO toDTO(Registo registo);
 
     List<RegistoDTO> toDTOList(List<Registo> registos);
+
+    RegistoClinicoDTO toClinicDTO(RegistoClinico registo);
+
+    List<RegistoClinicoDTO> toCLinicDTOList(List<RegistoClinico> registos);
 }
