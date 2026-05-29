c3d81d30d35efe612219c1aca56a173949fea375
1tsDan <2kupwhite@gmail.com>
2026-05-29 23:05:11 +0100

    fix: add model attribute annotation

--- src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/StatsController.java
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/controller/StatsController.java
@@ -1,12 +1,13 @@
 package pt.ipcb.kardex.kardex_eletronico.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import pt.ipcb.kardex.kardex_eletronico.controller.config.ApiResponse;
 import pt.ipcb.kardex.kardex_eletronico.controller.filter.ReportsFilter;
 import pt.ipcb.kardex.kardex_eletronico.dto.parametros_clinicos.CateterUsoDTO;
@@ -16,8 +17,6 @@ import pt.ipcb.kardex.kardex_eletronico.dto.stock.MedicamentoRankingDTO;
 import pt.ipcb.kardex.kardex_eletronico.dto.worker.FuncionarioAtividadeDTO;
 import pt.ipcb.kardex.kardex_eletronico.service.stats.StatsService;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/stats")
@@ -26,32 +25,55 @@ public class StatsController {
     private final StatsService service;
 
     @GetMapping("/counts")
-    public ResponseEntity<ApiResponse<KardexCountsDTO>> getCounts(HttpServletRequest request) {
+    public ResponseEntity<ApiResponse<KardexCountsDTO>> getCounts(
+        HttpServletRequest request
+    ) {
         var counts = service.getCounts(request);
-        return ResponseEntity.ok(ApiResponse.ok("Contagens encontradas com sucesso", counts));
+        return ResponseEntity.ok(
+            ApiResponse.ok("Contagens encontradas com sucesso", counts)
+        );
     }
 
     @GetMapping("/reports")
-    public ResponseEntity<ApiResponse<KardexReportsDTO>> getReports(ReportsFilter filter) {
+    public ResponseEntity<ApiResponse<KardexReportsDTO>> getReports(
+        @ModelAttribute ReportsFilter filter
+    ) {
         var reports = service.getReports(filter);
-        return ResponseEntity.ok(ApiResponse.ok("Relatorios encontradas com sucesso", reports));
+        return ResponseEntity.ok(
+            ApiResponse.ok("Relatorios encontradas com sucesso", reports)
+        );
     }
 
     @GetMapping("/medications/ranking")
-    public ResponseEntity<ApiResponse<List<MedicamentoRankingDTO>>> getMedicationsRanking(ReportsFilter filter) {
+    public ResponseEntity<
+        ApiResponse<List<MedicamentoRankingDTO>>
+    > getMedicationsRanking(@ModelAttribute ReportsFilter filter) {
         var reports = service.getMedicationsRanking(filter);
-        return ResponseEntity.ok(ApiResponse.ok("Relatorios encontradas com sucesso", reports));
+        return ResponseEntity.ok(
+            ApiResponse.ok("Relatorios encontradas com sucesso", reports)
+        );
     }
 
     @GetMapping("/workers/activity")
-    public ResponseEntity<ApiResponse<List<FuncionarioAtividadeDTO>>> getWorkersActivity(ReportsFilter filter) {
+    public ResponseEntity<
+        ApiResponse<List<FuncionarioAtividadeDTO>>
+    > getWorkersActivity(@ModelAttribute ReportsFilter filter) {
         var activities = service.getWorkerActivity(filter);
-        return ResponseEntity.ok(ApiResponse.ok("Atividade do funcionario encontrada com sucesso", activities));
+        return ResponseEntity.ok(
+            ApiResponse.ok(
+                "Atividade do funcionario encontrada com sucesso",
+                activities
+            )
+        );
     }
 
     @GetMapping("/cateters/usage")
-    public ResponseEntity<ApiResponse<List<CateterUsoDTO>>> getCateterUsage(ReportsFilter filter){
+    public ResponseEntity<ApiResponse<List<CateterUsoDTO>>> getCateterUsage(
+        @ModelAttribute ReportsFilter filter
+    ) {
         var usage = service.getCateterUsage(filter);
-        return ResponseEntity.ok(ApiResponse.ok("Uso de cateteres encontrado com sucesso", usage));
+        return ResponseEntity.ok(
+            ApiResponse.ok("Uso de cateteres encontrado com sucesso", usage)
+        );
     }
 }
