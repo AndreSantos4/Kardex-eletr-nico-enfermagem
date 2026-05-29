564d7a7e8ce90bb832c61afa42752a412e44df2b
1tsDan <2kupwhite@gmail.com>
2026-05-29 19:47:36 +0100

    feat: add enum to represent different types of clinic records

---
+++ src/main/java/pt/ipcb/kardex/kardex_eletronico/model/enumerated/TipoRegistoClinico.java
@@ -0,0 +1,11 @@
+package pt.ipcb.kardex.kardex_eletronico.model.enumerated;
+
+public enum TipoRegistoClinico {
+    PRESCRICAO,
+    ADMINISTRACAO,
+    EXAME,
+    SINAL_VITAL,
+    CATETER,
+    INCIDENTE,
+    CONTENCAO
+}
